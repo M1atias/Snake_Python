1834625e44bbab7b42c7827151c0a7d538321cfa
M1atias <maty.f.molina4a@gmail.com>
2022-02-22 11:40:53 -0300

    add:se grafican las frutas

--- snake.py
+++ snake.py
@@ -1,11 +1,31 @@
-import pygame,sys
+import pygame,sys,random
+from pygame.math import Vector2
+
+class FRUIT:
+    def __init__(self):
+        self.x = random.randint(0,cell_numbre -1 )
+        self.y = random.randint(0,cell_numbre -1 )
+        self.pos = pygame.math.Vector2(self.x,self.y)
+        #create an x and y position
+    
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
+        pygame.draw.rect(screen,(126,166,114),fruit_rect)
 
 pygame.init()
-screen = pygame.display.set_mode((400,500))
+cell_size = 30
+cell_numbre = 20
+screen = pygame.display.set_mode((cell_numbre*cell_size,cell_numbre*cell_size))
+clock = pygame.time.Clock()
+
+fruit = FRUIT()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    screen.fill((175,215,70))
+    fruit.draw_fruit()
     pygame.display.update()
+    clock.tick(60)
